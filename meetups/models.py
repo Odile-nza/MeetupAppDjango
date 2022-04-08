b702ab17ac8ff06f6fa4ca37e5d9ed0c4f1dd818
Odile <odille1998@gmail>
2022-04-08 11:40:27 +0200

    adding location model

--- meetups/models.py
+++ meetups/models.py
@@ -1,13 +1,24 @@
 from distutils.command.upload import upload
 from email.mime import image
 from pyexpat import model
+from unicodedata import name
 from django.db import models
+from django.forms import CharField
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to = 'images')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     
     def __str__(self):
         return f'{self.title} - {self.slug}'
