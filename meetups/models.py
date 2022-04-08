180d9952c5c4745cd168866a8c29baf18c34cb2e
Odile <odille1998@gmail>
2022-04-08 13:01:08 +0200

    adding participant model

--- meetups/models.py
+++ meetups/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+import email
 from email.mime import image
 from pyexpat import model
 from unicodedata import name
@@ -13,12 +14,21 @@ class Location(models.Model):
     def __str__(self):
         return f'{self.name} ({self.address})'
 
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email        
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
+    organiser_email = models.EmailField()
+    date = models.DateField()
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to = 'images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant, blank=True)
     
     def __str__(self):
         return f'{self.title} - {self.slug}'
